2605e0c17b5cac0c8fbff4f65ccd3dee579df312
Roshan <roshanvinson@gmail.com>
2023-10-28 22:04:55 +0530

    setup and requirements

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## Machine learning project
+
+setup.py -> builds the application as a package itself
+the SRC folder behaves as a package
+
+requirements.txt -> takes the mentioned packages in the file and installs when the setup.py is installed
+
+init_.py -> in setup.py the find_packages funtion will go search for this init file and install as its package itself and builds.
+so if i install requirements.txt automatically it calls the setup.py and installs everything as package.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+hypen_e_dot = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    returns from the requirements.txt
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+
+    return requirements
+
+
+
+
+setup(
+    name= 'e2estudentperformance',
+    version='0.0.1',
+    author = 'RoshanC',
+    author_email='roshanvinson@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements .txt')
+)
